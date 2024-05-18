892f1259eb69cd1818b619254b0c8794fbb12da7
shashwat558 <tumbaktuhere@gmail.com>
2024-05-19 03:28:18 +0530

    Completed this simple project

--- DocsApp/src/Components/Card.jsx
+++ DocsApp/src/Components/Card.jsx
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from "react-icons/lu";
+import { IoClose } from "react-icons/io5";
+import { motion } from 'framer-motion'; 
 
-const Card = () => {
+const Card = ({data, refrence}) => {
+
+
+    
+
   return (
-    <div>
-        <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
+    
+        <motion.div drag dragConstraints={refrence} whileDrag={{scale: 1.2}} className='relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
 
             <FaRegFileAlt />
-            <p className='text-sm mt-5 leading-tight'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-            <div className='footer absolute bottom-0 px-8 w-full h-14
-            py-3 left-0'>
-                <div className='flex items-center justify-between mb-6'>
-                    <h5>.4mb</h5>
+            <p className='text-sm mt-5 leading-tight'>{data.desc}</p>
+            <div className='footer absolute bottom-0 w-full
+             left-0'>
+                <div className='flex items-center justify-between py-3 
+                px-8 mb-6'>
+                    <h5>{data.filesize}</h5>
                     <span className='w-8 h-8 bg-zinc-600 rounded-full flex items-center justify-center'>
-                    <LuDownload size='.8em' color='#fff' />
+                    {data.close ? <IoClose /> : <LuDownload size='.8em' color='#fff' /> }
+                    
                     </span>
                 </div>
+                {data.tag.isOpen && (
+                    <div className={`tag w-full p-4 bg-${data.tag.tagColor}-500 flex items-center justify-center`}>
+                    <h3>{data.tag.tagTitle}</h3>
+                    
+                </div>
+
+                )
+                }
+                
             </div>
-        </div>
-    </div>
+        </motion.div>
+    
   )
 }
 
--- DocsApp/src/Components/Foreground.jsx
+++ DocsApp/src/Components/Foreground.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 const Foreground = () => {
+    // const data = [icon, desc, filesize, closeOrDownlaod, tagDetails];
+
+    const ref = useRef(null);
+
+    const data = [
+        {desc: "hey there i am teeku you are not teeku", 
+        filesize: ".9mb",
+        close: true, 
+        tag: {isOpen:true, tagTitle: "Download Now", tagColor: "green"},
+    },
+    {desc: "hey there i am teeku you are not teeku", 
+    filesize: ".9mb",
+    close: true, 
+    tag: {isOpen:true, tagTitle: "Download Now", tagColor: "green"}},
+    {desc: "hey there i am teeku you are not teeku", 
+    filesize: ".9mb",
+    close: true, 
+    tag: {isOpen:true, tagTitle: "Download Now", tagColor: "green"},
+},
+    ]
   return (
+    
     <div>
-         <div className='fixed z-[3] top-0 left-0 w-full h-full'>
-            <Card />
+         <div ref={ref} className='fixed z-[3] top-0 left-0 w-full h-full flex gap-10 flex-wrap'>
+            {data.map((item, index) => (
+                <Card data={item} refrence={ref}/>
+            ))}
+            
          </div>
     </div>
   )
